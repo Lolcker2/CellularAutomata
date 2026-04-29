956c7a8d0c073d85de4a4a5a271b14026acef5b8
Lolcker2 <eyal.locker2006@gmail.com>
2026-04-29 18:26:44 +0300

    spagetti

--- defs.js
+++ defs.js
@@ -6,43 +6,68 @@ const TOPOLOGY_TYPE = Enum({
 });
 
 
+const chord_TYPE = Enum({
+    RADIUS: 0,
+    DIAMETER: 1
+});
+
+
+// given a chord and a number of dimentions
+// generates all cartesian products within the specified chord,
+// the cord is treated differently depending on whether its a radius or a diameter
+// if it's a diameter the products are treated as absolute while for a radius they're
+//  treated as relative offsets from the midpoint
+
+const CartProducts = (chord, dim, chordType) => 
+{   
+    const radius = chordType === chord_TYPE.RADIUS ? chord : Math.floor(chord/2)
+    const LineOffset = Array.from({ length: chord%2? radius*2+1 :radius*2 }, (_, i) => chordType === chord_TYPE.RADIUS ? i - radius: i);
+    switch(dim)
+    {
+        case 1:
+            return LineOffset;
+        case 2:
+            return LineOffset.flatMap(x => LineOffset.map(y => [x, y]));
+        case 3:
+            return LineOffset.flatMap(x => LineOffset.map(y => LineOffset.map(z => [x, y, z]))).flat();
+    }
+}
+
+
+
 
 class CA
 {
     CreateLattice()
     {
-        //this.size is a index-dim pair where
-        // index is the ordinal number of the dimension
-        // dim is the size along that dimension
         let elem = this.stateSet[0];
-
-        this.sizes.forEach(dim => {
+        const sizes = Array(this.sizes[1]).fill(this.sizes[0]);
+        sizes.forEach(dim => {
             elem = Array(dim).fill(elem);
         });
         return elem;
     }
 
+
+    NeighborhoodSelection()
+    {
+
+
+        try {
+        // Risky code you want to ignore if it fails
+        riskyAction();
+        } catch {
+        // Empty block: execution just continues after this
+        }
+
+    }
+
+
     Run()
     {   
-        // agg is the total number of cells in the multi-dim grid
-        let agg = this.sizes.reduce((partialMul, a) => partialMul * a, 1);
-        let indecies = Array(this.sizes).fill(0); // coord vector
-
-        //foreach
-        for(let i = 0; i < agg; i++)
-        {
-            
-            // updating coords
-            for(let j = 0; j < indecies.length -1; j++)
-            {
-                if(indecies[j] == this.sizes[j])
-                {
-                    indecies[j] = 0;
-                    indecies[j+1]++;
-                }
-            }
-            indecies[0]++
-        }
+        CartProducts(this.sizes[0], this.sizes[1], chord_TYPE.DIAMETER).forEach(coordVector => {
+            console.log(coordVector);
+        });
     }
 
     constructor(stateSet, sizes, transitionFunction) //neighborhood, transition
@@ -55,6 +80,8 @@ class CA
         this.top = TOPOLOGY_TYPE.RECT; // torus or sqaure
 
         this.lattice = this.CreateLattice();
+        console.log(this.lattice);
+        this.Run();
     }
 }
 
@@ -63,9 +90,9 @@ class CA
 var gol = (x, ...args) => {let sum = args.reduce((partialSum, a) => partialSum + a, 0); return sum == 3 || (sum==2 && x);}
 
 
-var test = new CA([0, 1], [5, 5], gol);
+var test = new CA([0, 1], [3, 2], gol);
 
-
+// changing [5, 5] to [5, 2] and [5, 5, 5] to [5, 3]
 // make run work
 // get wrapp done
 // test
--- traversal.js
+++ traversal.js
@@ -11,3 +11,4 @@ class StateSpace
         this.arcs = arcs;
     }
 }
+
